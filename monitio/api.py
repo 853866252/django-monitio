949472c1fef225d93cd7441631192f2af3814116
Michał Pasternak <michal.dtz@gmail.com>
2013-06-16 21:38:49 +0200

    Update api, so SSE is *optional* and not default

--- monitio/api.py
+++ monitio/api.py
@@ -2,44 +2,48 @@ from monitio import notify
 from monitio import constants
 
 def add_message(request, level, message, extra_tags='', fail_silently=False,
-                subject='', user=None, email=False, sse=True, from_user=None,
+                subject='', user=None, email=False, sse=False, from_user=None,
                 expires=None, close_timeout=None):
     return request._messages.add(level, message, extra_tags, subject, user,
-                                 from_user, expires, close_timeout)
+                                 from_user, expires, close_timeout,
+                                 sse, email)
 
 
 def info(request, message, extra_tags='', fail_silently=False, subject='',
-         user=None, email=False, sse=True, from_user=None, expires=None,
+         user=None, email=False, sse=False, from_user=None, expires=None,
          close_timeout=None):
     """
     """
     level = constants.INFO
     return add_message(request, level, message, extra_tags, fail_silently,
-                       subject, user, email, sse, from_user, expires, close_timeout)
+                       subject, user, email, sse, from_user, expires,
+                       close_timeout)
 
 
 def warning(request, message, extra_tags='', fail_silently=False, subject='',
-            user=None, email=False, sse=True, from_user=None, expires=None,
+            user=None, email=False, sse=False, from_user=None, expires=None,
             close_timeout=None):
     """
     """
     level = constants.WARNING
     return add_message(request, level, message, extra_tags, fail_silently,
-                       subject, user, email, sse, from_user, expires, close_timeout)
+                       subject, user, email, sse, from_user, expires,
+                       close_timeout)
 
 
 def debug(request, message, extra_tags='', fail_silently=False, subject='',
-          user=None, email=False, sse=True, from_user=None, expires=None,
+          user=None, email=False, sse=False, from_user=None, expires=None,
           close_timeout=None):
     """
     """
     level = constants.DEBUG
     return add_message(request, level, message, extra_tags, fail_silently,
-                       subject, user, email, sse, from_user, expires, close_timeout)
+                       subject, user, email, sse, from_user, expires,
+                       close_timeout)
 
 
 def create_message(to_user, level, message, from_user=None, extra_tags='',
-                   subject='', expires=None, close_timeout=None, sse=True,
+                   subject='', expires=None, close_timeout=None, sse=False,
                    email=False):
     """
     Use this method to create message without a request object - this can
