8bc16ed2e71c65d446010377fa73bf2a450f86f2
Michał Pasternak <michal.dtz@gmail.com>
2013-09-29 23:58:04 +0200

    Update the hack for LINUX

--- monitio/views.py
+++ monitio/views.py
@@ -97,7 +97,7 @@ class DynamicChannelRedisQueueView(RedisQueueView):
             msg = u''
             for bufferitem in self.sse:
                 msg = msg + bufferitem
-            yield msg + (" " * (1024-len(msg)))
+            yield msg + (" " * (4096-len(msg)))
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
