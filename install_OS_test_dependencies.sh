24eff9a37bf4c90bdea12b9efc5929652d3bd537
Fábio C. Barrioneuvo da Luz <bnafta@gmail.com>
2014-04-04 18:09:30 -0300

    another possible fix to chrome on travis

--- install_OS_test_dependencies.sh
+++ install_OS_test_dependencies.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# remove chromium-browser
+sudo mkdir -p /usr/share/desktop-directories
+sudo apt-get remove chromium-browser
 echo "installing browsers..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable firefox
+sudo apt-get install -f
 echo "downloading chromedriver..."
 wget http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip
