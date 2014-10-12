bb93fa6637d9938c999c74e3746a5e5a26defc06
Michał Pasternak <michal.dtz@gmail.com>
2014-10-12 12:58:08 +0200

    Small fixes

--- test_project/manage.py
+++ test_project/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+import sys
+
+# Add ../monitio to sys.path
+
+sys.path = [ os.path.join(os.path.dirname(__file__), '..'), ] + sys.path
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 
